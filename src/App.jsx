15041a3807836d4e4ef30e5342b1a07e4dc08b06
Samiuzzaman Chowdhury <chowdhurysamiuzzaman@gmail.com>
2025-07-16 17:07:44 +0600

    Setup Tailwind CSS, React Router Dom, Shadcn UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
-      <div>
-        <h1>This is Work Panel Project</h1>
-        <p>Complete Web Development Assigment 12 Project</p>
+      <div className="h-screen gap-10 flex flex-col justify-center items-center">
+        <h1 className="text-green-700 font-semibold text-5xl">
+          This is Work Panel Project
+        </h1>
+        <p className="text-red-600 font-medium text-2xl">
+          Complete Web Development Assigment 12 Project
+        </p>
+        <Button>Click me</Button>
       </div>
     </>
   );
