9e6f9ee390182cf9f77a719b3ae122c9f38d5ea1
Samiuzzaman Chowdhury <chowdhurysamiuzzaman@gmail.com>
2025-07-16 16:29:42 +0600

    Ready to Build the Work Panel Client Side

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1>This is Work Panel Project</h1>
+        <p>Complete Web Development Assigment 12 Project</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
